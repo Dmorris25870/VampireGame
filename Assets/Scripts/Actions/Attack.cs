a43c778d37449dd35fe3d5cb9b93aea1b97c2fd3
Daniel Lloyd <1027587@student.sae.edu.au>
2024-03-27 16:46:07 +1000

    Starting to implement the attack action
    + Added AttackActionScene for testing.
    + Added and still implementing Attack action.

---
+++ Assets/Scripts/Actions/Attack.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VartaAbyss.Actions
+{
+	public class Attack : MonoBehaviour
+	{
+		//private EnemyBase m_target;
+		//private bool m_isCurrentlyAttacking;
+		//private Weapon m_currentSelectedWeapon;
+
+		//public EnemyBase Target { get { return m_target; } }
+		//public Weapon CurrentSelectedWeapon { get { return m_currentSelectedWeapon; } }
+
+		//private DamageType GetDamageType(Weapon attackType)
+		//{
+		//	return CurrentSelectedWeapon;
+		//}
+
+		//private void Attack(EnemyBase target)
+		//{
+		//	m_isCurrentlyAttacking = true;
+		//	target.health -= GetDamageType(VartaAbyss.Player.CurrentWeapon);
+		//}
+
+		//private void AddToQueue()
+		//{
+			
+		//}
+	}
+}
