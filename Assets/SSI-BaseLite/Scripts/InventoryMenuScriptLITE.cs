8dae89c9a7fad085a5c945ef4c376aa29d94f91b
Daniel Lloyd <1027587@student.sae.edu.au>
2024-04-01 11:03:52 +1000

    Encased Inventory feature in namespace VartaAbyss.Inventory
    ~ Went through Inventory systems and adjusted them to follow coding standards
    ~ Inventory feature is now found under Inventory namespace.

--- Assets/SSI-BaseLite/Scripts/InventoryMenuScriptLITE.cs
+++ Assets/SSI-BaseLite/Scripts/InventoryMenuScriptLITE.cs
@@ -4,33 +4,42 @@ using UnityEngine;
 
 //the only purpose of this script is to lock/unlock the mouse cursor and disable player movement if you're using the RigidbodyFPSController (such as in the sample scene)
 
-public class InventoryMenuScriptLITE : MonoBehaviour {
+namespace VartaAbyss.Inventory
+{
+	public class InventoryMenuScriptLITE : MonoBehaviour
+	{
+		private bool inInventory = false;
+		public Transform playerObj;
+		public KeyCode inventoryKey;
 
-	private bool inInventory = false;
-	public Transform playerObj;
-	public KeyCode inventoryKey;
+		private void Start()
+		{
+			inventoryKey = GetComponent<InventoryScriptLITE>().inventoryKey;
 
-	void Start (){
-		inventoryKey = GetComponent<InventoryScriptLITE> ().inventoryKey;
-		if (playerObj == null) {
-			playerObj = transform.parent.parent; //this is assuming the Inventory object is in the MainCamera object
+			if ( playerObj == null )
+			{
+				playerObj = transform.parent.parent; //this is assuming the Inventory object is in the MainCamera object
+			}
 		}
-	}
 
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown (inventoryKey)) {
-			inInventory = !inInventory;
-			if (inInventory) {
-				playerObj.GetComponent<MonoBehaviour> ().enabled = false;
-				Cursor.lockState = CursorLockMode.None;
-				Cursor.visible = true;
-			} else {
-				playerObj.GetComponent<MonoBehaviour> ().enabled = true;
-				Cursor.lockState = CursorLockMode.Locked;
-				Cursor.visible = false;
+		private void Update()
+		{
+			if ( Input.GetKeyDown(inventoryKey) )
+			{
+				inInventory = !inInventory;
+				if ( inInventory )
+				{
+					playerObj.GetComponent<MonoBehaviour>().enabled = false;
+					Cursor.lockState = CursorLockMode.None;
+					Cursor.visible = true;
+				}
+				else
+				{
+					playerObj.GetComponent<MonoBehaviour>().enabled = true;
+					Cursor.lockState = CursorLockMode.Locked;
+					Cursor.visible = false;
+				}
 			}
 		}
 	}
 }
-
