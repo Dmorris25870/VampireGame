80dc2f680b26bf648fe99b7369f5878d11e8b369
Daniel Lloyd <1027587@student.sae.edu.au>
2024-04-06 15:13:42 +1000

    Implementing Dialogue System and imported basic sprites for UI
    + An asset that contains free sprites used for UI
    + Implementing dialogue UI and system

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace VartraAbyss.Dialogue
+{
+	public class Dialogue : MonoBehaviour
+	{
+		[SerializeField] private string m_speakerName;
+		[SerializeField] private Image m_speakerPortrait;
+
+		[TextArea(10,20)]
+		[SerializeField] private string m_dialogue;
+	}
+}
