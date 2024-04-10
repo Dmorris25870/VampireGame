21f5a0e45677ee51209ada28525a79856cd11ea9
Daniel Lloyd <1027587@student.sae.edu.au>
2024-04-10 12:31:44 +1000

    Setting up a Test Bed for the Stats and Levelling Systems
    + Basic testing suite to check if adding xp works, if levelling up works, stat distribution

---
+++ Assets/Scripts/Stats/LevellingSystem.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VartraAbyss.Stats
+{
+	public class LevellingSystem : MonoBehaviour
+	{
+		[SerializeField] private int m_currentLevel;
+		[SerializeField] private int m_currentXp;
+		[SerializeField] private int m_currentXPThreshold;
+		[SerializeField] private float m_xpThresholdMultiplier;
+
+		public int CurrentLevel { get { return m_currentLevel; } }
+		public int CurrentXP { get { return m_currentXp; } }
+		public int CurrentXPThreshold { get { return m_currentXPThreshold; } }
+		public float XPThresholdMultiplier { get { return m_xpThresholdMultiplier; } }
+
+		public void AddXP(int xpGained)
+		{
+			m_currentXp += xpGained;
+
+			if( m_currentXp >= m_currentXPThreshold )
+			{
+				LevelUp();
+			}
+
+		}
+
+		private void LevelUp()
+		{
+			int tempCurrentThreshold;
+
+			m_currentLevel++;
+			m_currentXp -= m_currentXPThreshold;
+			tempCurrentThreshold = (int)( (float)m_currentXPThreshold * (float)m_xpThresholdMultiplier );
+			m_currentXPThreshold = tempCurrentThreshold;
+			//uiManager.ShowLevelUI();
+
+			if( m_currentXp >= m_currentXPThreshold )
+			{
+				LevelUp();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Stats/StatSystem.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace VartraAbyss.Stats
+{
+	public class StatSystem : MonoBehaviour
+	{
+		[SerializeField] private int m_vitality;
+		[SerializeField] private int m_mind;
+		[SerializeField] private int m_strength;
+		[SerializeField] private int m_dexterity;
+
+		[SerializeField] private float m_vitalityMultiplier;
+		[SerializeField] private float m_mindMultiplier;
+		[SerializeField] private float m_strengthMultiplier;
+		[SerializeField] private float m_dexterityMultiplier;
+
+		[SerializeField] private int m_initialStatPointPool;
+		[SerializeField] private int m_currentStatPointPool;
+		[SerializeField] private int m_statPointPoolOnLevelUp;
+
+		public void AssignStats()
+		{
+			m_currentStatPointPool = m_initialStatPointPool;
+
+			m_vitality = Random.Range(0 , m_currentStatPointPool);
+			m_currentStatPointPool -= m_vitality;
+
+			m_mind = Random.Range(0 , m_currentStatPointPool);
+			m_currentStatPointPool -= m_mind;
+
+			m_strength = Random.Range(0 , m_currentStatPointPool);
+			m_currentStatPointPool -= m_strength;
+
+			m_dexterity = Random.Range(0 , m_currentStatPointPool);
+			m_currentStatPointPool -= m_dexterity;
+			Logger();
+		}
+
+		public void CalculateDancingStats()
+		{
+			//rhythm = (int)( (float)strength * (float)strengthMultiplier );
+			//style = (int)( (float)agility * (float)agilityMultiplier );
+			//luck = (int)( (float)intelligence * (float)intelligenceMultiplier );
+		}
+
+		public void DistributePhysicalStatsOnLevelUp(int PointsPool)
+		{
+			//PointsPool = myPointsPool;
+
+			//int tempStrength = PointsPool / myFirstPointDistributer;
+			//strength += tempStrength;
+			//PointsPool -= tempStrength;
+			//int tempAgility = PointsPool / mySecondPointDistributer;
+			//agility += tempAgility;
+			//PointsPool -= tempAgility;
+			//int tempIntelligence = PointsPool;
+			//intelligence += tempIntelligence;
+
+			CalculateDancingStats();
+		}
+
+		private void Logger()
+		{
+			Debug.Log($"Player Vitality is: {m_vitality}. \n Player Mind is: {m_mind}. \n Player Strength is: {m_strength}. \n Player Dexterity is: {m_dexterity}.");
+		}
+	}
+}
