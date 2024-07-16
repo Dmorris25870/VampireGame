89fe0879b137b77b15cf7f0c95ded57c129dc187
SAE1\1024949 <1024949@student.sae.edu.au>
2024-07-16 11:19:47 +1000

    Set up basic 4-directional movement anims

--- Assets/_VartraAbyss/Scripts/UI/SpriteToCamera.cs
+++ Assets/_VartraAbyss/Scripts/UI/SpriteToCamera.cs
@@ -6,12 +6,13 @@ namespace VartraAbyss.UI
 	{
 		public Vector3 lockedRotation = new Vector3(0 , -45 , 0);
 		[SerializeField] private GameObject m_sprite;
-
+				
 		void LateUpdate()
 		{
-			
+
 			// Lock the rotation to the specified values
 			m_sprite.transform.rotation = Quaternion.Euler(lockedRotation);
+			
 		}
 	}
 }
