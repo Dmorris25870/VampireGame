10bd7d05f67615fd63becba3774db9938690f95d
Daniel Lloyd <1027587@student.sae.edu.au>
2024-08-15 15:49:49 +1000

    Fixed walking animation to use updated spritesheet

--- Assets/_VartraAbyss/Scripts/Animations/PlayerAnimations.cs
+++ Assets/_VartraAbyss/Scripts/Animations/PlayerAnimations.cs
@@ -17,37 +17,110 @@ namespace VartraAbyss
 		{
 			m_playerNotMoving = new Vector3(0 , 0 , 0);
 		}
+
 		private void Update()
 		{
-			if(player.velocity == m_playerNotMoving)
+			Vector3 velocity = player.velocity.normalized; // Normalize the velocity
+
+			if(velocity == m_playerNotMoving)
 			{
+				anim.SetBool("isMoving" , false);
 				StopWalkAnim();
 			}
+			else
+			{
+				anim.SetBool("isMoving" , true);
+				string direction = GetDirection(velocity);
+				UpdateAnimation(direction);
+			}
 		}
-
 		private void StopWalkAnim()
 		{
 			anim.Play("PlayerIdle");
 		}
-		public void PlayWalkAnim()
+
+		string GetDirection(Vector3 velocity)
 		{
-			if(x < 0 & z < 0)
+			Vector3 roundedVelocity = new Vector3(Mathf.Round(velocity.x) , Mathf.Round(velocity.z));
+
+			// Use a switch statement to map the rounded velocity to a direction
+			switch(roundedVelocity)
 			{
-				sr.flipX = false;
-				anim.Play("PlayerWalkLeft");
+				case Vector3 direction when direction.x > 0 && direction.y > 0:
+				return "Northeast";
+				case Vector3 direction when direction.x < 0 && direction.y > 0:
+				return "Northwest";
+				case Vector3 direction when direction.x > 0 && direction.y < 0:
+				return "Southeast";
+				case Vector3 direction when direction.x < 0 && direction.y < 0:
+				return "Southwest";
+				case Vector3 direction when direction.x > 0 && Mathf.Approximately(direction.y , 0):
+				return "East";
+				case Vector3 direction when direction.x < 0 && Mathf.Approximately(direction.y , 0):
+				return "West";
+				case Vector3 direction when Mathf.Approximately(direction.x , 0) && direction.y > 0:
+				return "North";
+				case Vector3 direction when Mathf.Approximately(direction.x , 0) && direction.y < 0:
+				return "South";
+				default:
+				return "Idle";
 			}
-			else if(x > 0 & z > 0)
+		}
+
+		void UpdateAnimation(string direction)
+		{
+			switch(direction)
 			{
+				case "Northeast":
 				sr.flipX = true;
-				anim.Play("PlayerWalkRight");
-			}
-			else if(x < 0 & z > 0)
-			{
-				anim.Play("PlayerWalkUp");
-			}
-			else if(x > 0 & z < 0)
-			{
-				anim.Play("PlayerWalkDown");
+				anim.SetFloat("Horizontal" , 1f);
+				anim.SetFloat("Vertical" , 1f);
+				break;
+
+				case "Northwest":
+				sr.flipX = false;
+				anim.SetFloat("Horizontal" , -1f);
+				anim.SetFloat("Vertical" , 1f);
+				break;
+
+				case "Southeast":
+				sr.flipX = true;
+				anim.SetFloat("Horizontal" , 1f);
+				anim.SetFloat("Vertical" , -1f);
+				break;
+
+				case "Southwest":
+				sr.flipX = false;
+				anim.SetFloat("Horizontal" , -1f);
+				anim.SetFloat("Vertical" , -1f);
+				break;
+
+				case "East":
+				sr.flipX = true;
+				anim.SetFloat("Horizontal" , 1f);
+				anim.SetFloat("Vertical" , 0f);
+				break;
+
+				case "West":
+				sr.flipX = false;
+				anim.SetFloat("Horizontal" , -1f);
+				anim.SetFloat("Vertical" , 0f);
+				break;
+
+				case "North":
+				anim.SetFloat("Horizontal" , 0f);
+				anim.SetFloat("Vertical" , 1f);
+				break;
+
+				case "South":
+				anim.SetFloat("Horizontal" , 0f);
+				anim.SetFloat("Vertical" , -1f);
+				break;
+
+				default: // Idle case
+				anim.SetFloat("Horizontal" , 0f);
+				anim.SetFloat("Vertical" , 0f);
+				break;
 			}
 		}
 	}
